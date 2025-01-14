c29b4e168c45be5e2a6506fb27a6e940b41102ba
jeanelleym <jymcleod@unc.edu>
2025-01-14 13:54:17 -0500

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,9 @@
+"""My first excercise in COMP110!"""
+
+__author__ ="730572910"
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+if __name__ == "__main__":
+    print(greet(name=input("What is your name? ")))
